a54b5bb3a8184907467c139ad36bd3e8e99f3e5d
Danil <kl1m.done@gmail.com>
2020-04-25 19:54:27 +0300

    adds contacts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,13 @@
+$('.promo-nav-burger').click(function () {
+  $('.promo-nav-list-mobile').toggleClass('nav-closed');
+
+  if ($('.promo-nav-list-mobile').hasClass('nav-closed')) {
+    $('.promo-nav-burger').removeClass('nav-burger-closed');
+  } else {
+    $('.promo-nav-burger').addClass('nav-burger-closed');
+  }
+});
+
 $('.cases-slider').slick({
   infinite: true,
   //autoplay: true,
@@ -25,11 +35,35 @@ $('.about-photo').slick({
   slidesToScroll: 1,
   arrows: false,
   dotsClass: 'slider-dots',
-  customPaging : function(slider, i) {
+  customPaging: function (slider, i) {
     return '';
-    },
+  },
   fade: true,
   focusOnSelect: true,
+  pauseOnHover: false,
+  //adaptiveHeight: true,
+});
+
+$('.about-btn-goit').click(function () {
+  var buttonId = $(this).attr('id');
+  $('#modal-container-goit').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+});
+
+$('.about-btn-cisco').click(function () {
+  var buttonId = $(this).attr('id');
+  $('#modal-container-cisco').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+});
+
+$('#modal-container-goit').click(function () {
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
+
+$('#modal-container-cisco').click(function () {
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
 });
 
 // $('.btn-sites').click(function () {
